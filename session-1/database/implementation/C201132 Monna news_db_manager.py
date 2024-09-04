a35e6f26e33d25cebf1ebfe488562442ff7ccf86
Helal Faruk Monna <112116625+monna-bhai@users.noreply.github.com>
2024-09-04 13:05:19 +0600

    Update and rename news_db_manager.py to C201132 Monna news_db_manager.py

--- session-1/database/implementation/C201132 Monna news_db_manager.py
+++ session-1/database/implementation/C201132 Monna news_db_manager.py
@@ -1,3 +1,4 @@
+
 import os
 import mysql.connector
 from mysql.connector import Error
@@ -22,7 +23,7 @@ def create_db_connection():
             passwd=os.getenv("DB_PASS"),
             database=os.getenv("DB_NAME")
         )
-        print("MySQL Database connection successful")
+        print("MySQL Database connection successful! Done by- Helal Faruk Monna, C201132!")
         return connection
     except Error as e:
         print(f"The error '{e}' occurred")
@@ -73,7 +74,7 @@ def execute_read_query(connection, query):
         result = cursor.fetchall()
         return result
     except Error as e:
-        print(f"The error '{e}' occurred")
+        print(f"The error '{e}' occurred!")
         return []
 
 def create_tables(connection):
@@ -96,33 +97,41 @@ def create_tables(connection):
         description TEXT
     );
     """
-    create_authors_table = """
-    CREATE TABLE IF NOT EXISTS authors (
+    create_repoters_table = """
+    CREATE TABLE IF NOT EXISTS repoters (
         id INT AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         email VARCHAR(255) UNIQUE NOT NULL
     );
     """
-    create_editors_table = """
-    CREATE TABLE IF NOT EXISTS editors (
+    create_publishers_table = """
+    CREATE TABLE IF NOT EXISTS publishers (
         id INT AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) UNIQUE NOT NULL
+        email VARCHAR(255) UNIQUE NOT NULL,
+        phone_number VARCHAR(255) NOT NULL,
+        head_office_address VARCHAR(255) NOT NULL,
+        website VARCHAR(255) NOT NULL,
+        facebook VARCHAR(255) NOT NULL,
+        twitter VARCHAR(255)NOT NULL,
+        linkedin VARCHAR(255)NOT NULL,
+        instagram VARCHAR(255) NOT NULL
     );
     """
+    
     create_news_table = """
     CREATE TABLE IF NOT EXISTS news (
         id INT AUTO_INCREMENT PRIMARY KEY,
         category_id INT,
-        author_id INT,
-        editor_id INT,
+        reporter_id INT,
+        publisher_id INT,
         datetime DATETIME,
         title VARCHAR(255) NOT NULL,
         body TEXT,
         link VARCHAR(255),
         FOREIGN KEY (category_id) REFERENCES categories (id),
-        FOREIGN KEY (author_id) REFERENCES authors (id),
-        FOREIGN KEY (editor_id) REFERENCES editors (id)
+        FOREIGN KEY (reporter_id) REFERENCES repoters (id),
+        FOREIGN KEY (publisher_id) REFERENCES publishers (id)
     );
     """
     create_images_table = """
@@ -142,8 +151,8 @@ def create_tables(connection):
     );
     """
     execute_query(connection, create_categories_table)
-    execute_query(connection, create_authors_table)
-    execute_query(connection, create_editors_table)
+    execute_query(connection, create_repoters_table)
+    execute_query(connection, create_publishers_table)
     execute_query(connection, create_news_table)
     execute_query(connection, create_images_table)
     execute_query(connection, create_summaries_table)
@@ -153,12 +162,24 @@ def create_tables(connection):
 if __name__ == "__main__":
     conn = create_db_connection()
     if conn is not None:
-        # create_tables(conn)
+        create_tables(conn)
+        
         read_categories_query = "SELECT * FROM categories"
         news_categories = execute_read_query(conn, read_categories_query)
         print(news_categories)
 
-        read_authors_query = "SELECT * FROM authors"
-        news_authors = execute_read_query(conn, read_authors_query)
-        print(news_authors)
-
+        read_repoters_query = "SELECT * FROM repoters"
+        news_repoters = execute_read_query(conn, read_repoters_query)
+        print(news_repoters)
+        
+        read_publishers_query = "SELECT * FROM publishers"
+        news_publishers = execute_read_query(conn, read_publishers_query)
+        print(news_publishers)
+        
+        read_news_query = "SELECT * FROM news"
+        news = execute_read_query(conn, read_news_query)
+        print(news)
+        
+        read_images_query = "SELECT * FROM images"
+        news_images = execute_read_query(conn, read_images_query)
+        print(news_images)
